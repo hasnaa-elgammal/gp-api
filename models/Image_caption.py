ef55b481199820ff981c4ec619d2882b8b51afb1
Rawan_Elbanna <ciaorona633@gmail.com>
2023-03-15 15:24:45 +0200

    Update Image_caption.py

--- models/Image_caption.py
+++ models/Image_caption.py
@@ -4,34 +4,24 @@ from PIL import Image
 
 
 def imageCaption_predict(image_paths):
-    try:
-        model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-        feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-        tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        model.to(device)
+    model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
-        max_length = 16
-        num_beams = 4
-        gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
-        images = []
-        for image_path in image_paths:
-            i_image = Image.open(image_path)
-            if i_image.mode != "RGB":
-                i_image = i_image.convert(mode="RGB")
+    max_length = 16
+    num_beams = 4
+    gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+    img = image_paths.convert("RGB")
 
-            images.append(i_image)
+    pixel_values = feature_extractor(images=img, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
 
-        pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
-        pixel_values = pixel_values.to(device)
+    output_ids = model.generate(pixel_values, **gen_kwargs)
 
-        output_ids = model.generate(pixel_values, **gen_kwargs)
-
-        preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-        preds = [pred.strip() for pred in preds]
-        return preds
-
-    except:
-        return "Error. Please try again."
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+    return preds
 
 
